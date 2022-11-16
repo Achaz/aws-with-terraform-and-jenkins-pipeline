9e9b3969e44ba5263f4aef31b0d2ecfc236e6c2c
Achaz <jtugume123@gmail.com>
2022-11-16 13:51:16 +0300

    tried to make some changes to the scripts

--- subnets.tf
+++ subnets.tf
@@ -1,18 +1,16 @@
 locals {
-  availability_zones = "${var.region}a"
+    availability_zones = "${var.region}a"
 }
-
 resource "aws_subnet" "public-subnet-1" {
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id     = data.aws_vpc.existing_vpc.id
     cidr_block = var.cidr_blocks[1].cidr_block
     availability_zone = local.availability_zones
     tags = {
         Name = "${lower(var.vendor)}-${lower(var.environment)}-public-${local.availability_zones}"
     }
 }
-
 resource "aws_subnet" "private-subnet-1" {
-    vpc_id = data.aws_vpc.existing_vpc.id
+    vpc_id     = data.aws_vpc.existing_vpc.id
     cidr_block = var.cidr_blocks[2].cidr_block
     availability_zone = local.availability_zones
     tags = {
