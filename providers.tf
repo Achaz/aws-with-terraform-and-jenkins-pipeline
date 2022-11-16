f06d51e598e4bcf94201a2c5fa044d0b939c0261
Achaz <jtugume123@gmail.com>
2022-11-16 11:40:51 +0300

    removed the secret keys from the provider

--- providers.tf
+++ providers.tf
@@ -11,9 +11,8 @@ provider "aws" {
     endpoints {
         sts = "https://sts.us-east-1.amazonaws.com"
     }
-    access_key = "AKIA34WWEPRDNBGCUEMF"
-    secret_key = "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"
-    # access_key = var.access_key
-    # secret_key = var.secret_key
+    
+    access_key = var.access_key
+    secret_key = var.secret_key
     region     = "us-east-1"
 }
